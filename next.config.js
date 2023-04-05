06317de29995ea881b70f891e5512041d8eb9556
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-04-04 23:32:05 -0300

    Fetch to get articles from TechCrunch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'techcrunch.com',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import PageLayout from '@/components/PageLayout'
+import NewsList from '@/components/NewsList'
 
-export default function Home() {
+export default function Home(props) {
 	return (
 		<PageLayout title='Home'>
-			<h1>News App</h1>
+			<NewsList {...props} />
 		</PageLayout>
 	)
+}
+
+export async function getServerSideProps() {
+	const response = await fetch(
+		`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.API_KEY}`
+	)
+	const { articles } = await response.json()
+
+	return { props: { articles } }
 }
